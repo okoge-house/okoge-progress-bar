8893a4b33d7484db950c52ae3069c56245e1852d
GitHub Action <action@github.com>
2024-12-28 07:08:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/kkhouse777/okogeprogressbar/MyBundle.kt
+++ src/main/kotlin/com/github/kkhouse777/okogeprogressbar/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kkhouse777.okogeprogressbar
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kkhouse777/okogeprogressbar/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/kkhouse777/okogeprogressbar/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kkhouse777.okogeprogressbar.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
