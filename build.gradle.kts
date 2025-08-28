41f89dbb56e501877c47d27b9662eb3e40d547aa
kk-house-777 <intelinside.7575@gmail.com>
2025-08-28 21:19:59 +0900

    update 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ version = providers.gradleProperty("pluginVersion").get()
 
 // Set the JVM language level used to build the project.
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 // Configure project's dependencies
@@ -89,7 +89,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
